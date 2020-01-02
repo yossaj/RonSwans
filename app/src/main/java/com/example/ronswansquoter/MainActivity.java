205cabe164080adf104c605d2c42dab60c78230f
yossaj <yossaj@icloud.com>
2020-01-02 14:35:21 +0000

    single quote

--- app/src/main/java/com/example/ronswansquoter/MainActivity.java
+++ app/src/main/java/com/example/ronswansquoter/MainActivity.java
@@ -2,13 +2,50 @@ package com.example.ronswansquoter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.ronswansquoter.utilites.NetworkUtils;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mInitalQuote;
+    private String quote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mInitalQuote = (TextView)findViewById(R.id.inital_quote);
+        new QuoteRequest().execute();
+
+
+    }
+
+    public class QuoteRequest extends AsyncTask<URL, Void, String> {
+
+
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            String tempQuote = null;
+            try {
+                tempQuote = NetworkUtils.getResponseFromHttpUrl();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return tempQuote;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            mInitalQuote.setText(s);
+            super.onPostExecute(s);
+        }
     }
 }
--- app/src/main/java/com/example/ronswansquoter/utilites/NetworkUtils.java
+++ app/src/main/java/com/example/ronswansquoter/utilites/NetworkUtils.java
@@ -1,4 +1,32 @@
 package com.example.ronswansquoter.utilites;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
 public class NetworkUtils {
+
+    private static String swansonNet = "http://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+    public static String getResponseFromHttpUrl() throws IOException {
+        URL url = new URL(swansonNet);
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
 }
