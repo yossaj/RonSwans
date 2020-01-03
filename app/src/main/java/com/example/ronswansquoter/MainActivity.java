fe6352aad4a72f584e5d9c18554fc914361640ae
yossaj <yossaj@icloud.com>
2020-01-03 09:46:12 +0000

    recycler view working with one quote

--- app/src/main/java/com/example/ronswansquoter/MainActivity.java
+++ app/src/main/java/com/example/ronswansquoter/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.ronswansquoter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.example.ronswansquoter.utilites.Adapter;
 import com.example.ronswansquoter.utilites.NetworkUtils;
 
 import org.json.JSONException;
@@ -14,18 +18,30 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mInitalQuote;
+    private RecyclerView mRecyclerView;
+    private Adapter mAdapter;
+    private ArrayList<String> tempArray;
     private String quote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mInitalQuote = (TextView)findViewById(R.id.inital_quote);
+        mRecyclerView = (RecyclerView)findViewById(R.id.recyclerview_quoter);
+        mInitalQuote = (TextView)findViewById(R.id.error_message_display);
         newQuote(mInitalQuote);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        mAdapter = new Adapter();
+        mRecyclerView.setAdapter(mAdapter);
+        tempArray = new ArrayList<>();
 
 
     }
@@ -34,27 +50,38 @@ public class MainActivity extends AppCompatActivity {
         new QuoteRequest().execute();
     }
 
-    public class QuoteRequest extends AsyncTask<URL, Void, String> {
+    public class QuoteRequest extends AsyncTask<URL, Void, String[]> {
 
 
 
         @Override
-        protected String doInBackground(URL... urls) {
+        protected String[] doInBackground(URL... urls) {
             String tempQuote = null;
+
             try {
+                /* network response returns a string rather than a json object - this needs to be turned into a String[] in order for recycleView to accept it*/
                 tempQuote = NetworkUtils.getResponseFromHttpUrl();
+                tempArray.add(tempQuote);
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
-            return tempQuote;
+
+
+            String[] stringArrayQuotes = new String[tempArray.size()];
+
+            for(int i = 0; i < tempArray.size(); i++){
+                /*tempQuote = tempQuote.replaceAll("\\[","").replaceAll("\\]","");*/
+                stringArrayQuotes[i] = tempQuote;
+            }
+
+            return stringArrayQuotes;
         }
 
         @Override
-        protected void onPostExecute(String quote) {
-            quote = quote.replaceAll("\\[","").replaceAll("\\]","");
-            mInitalQuote.setText(quote);
-            super.onPostExecute(quote);
+        protected void onPostExecute(String[] quotes) {
+            mAdapter.setmQuoteArray(quotes);
+            super.onPostExecute(quotes);
         }
     }
 }
---
+++ app/src/main/java/com/example/ronswansquoter/utilites/Adapter.java
@@ -0,0 +1,62 @@
+package com.example.ronswansquoter.utilites;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.ronswansquoter.R;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.AdapterViewHolder> {
+
+    private String[] mQuoteArray;
+
+    public Adapter(){
+
+    }
+
+
+    public class AdapterViewHolder extends RecyclerView.ViewHolder{
+
+        public final TextView mQuoteHolderTextView;
+
+        public AdapterViewHolder(View itemView) {
+            super(itemView);
+            mQuoteHolderTextView = (TextView) itemView.findViewById(R.id.quote_items);
+        }
+    }
+
+    @NonNull
+    @Override
+    public AdapterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        int layoutIdForListItem = R.layout.quoter_list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutIdForListItem, parent, shouldAttachToParentImmediately);
+        return new AdapterViewHolder(view);
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull AdapterViewHolder holder, int position) {
+        String quoteOfTheMoment = mQuoteArray[position];
+        quoteOfTheMoment = quoteOfTheMoment.replaceAll("\\[","").replaceAll("\\]","");
+        holder.mQuoteHolderTextView.setText(quoteOfTheMoment);
+    }
+
+    @Override
+    public int getItemCount() {
+        if(mQuoteArray == null){ return 0;}
+        return mQuoteArray.length;
+    }
+
+    public void setmQuoteArray(String[] quoteArray) {
+        mQuoteArray = quoteArray;
+        notifyDataSetChanged();
+    }
+}
